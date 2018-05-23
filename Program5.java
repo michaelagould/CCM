82673b89f7ee09310bdec880fc84a3641a79d84e
Michael Gould <michael.a.gould@gmail.com>
2018-05-23 13:05:28 -0400

    Add files via upload

--- Program5.java
+++ Program5.java
@@ -5,9 +5,9 @@ public class Program5
 	public static void main(String[] args)
 	{
 	Scanner read = new Scanner(System.in);
-	System.out.println("How many courses are you taking this semester?");
+	System.out.print("How many courses are you taking this semester?:");
 	int num = read.nextInt();
-	System.out.println("Wow you are taking " + num + " classes!");
+	System.out.print("Wow you are taking " + num + " classes!");
 	read.close();
 	}
 }
--- Program6.java
+++ Program6.java
@@ -5,9 +5,9 @@ public class Program6
 	public static void main(String[] args)
 	{
 	Scanner read = new Scanner(System.in);
-	System.out.println("How many bottles of did you buy?");
+	System.out.print("How many bottles of did you buy?: ");
 	int bottles = read.nextInt();
-	System.out.println("How much did each bottle cost?");
+	System.out.print("How much did each bottle cost?: ");
 	int cost = read.nextInt();
 	int result = bottles * cost;
 	System.out.println("The cost of that many bottles at that price is: " + result);
